5410da23bde124a1d5168eaca0f135258ddfbebb
Dion Legierse <dal.legierse@student.avans.nl>
2019-06-17 22:55:15 +0200

    Update - removed all magic numbers

--- Visionb8/src/vision8b/dice_count.rs
+++ Visionb8/src/vision8b/dice_count.rs
@@ -3,6 +3,9 @@ use crate::vision8b::*;
 use crate::vision8b::license_plate::create_disk;
 
 pub fn count_eyes(path : &str) -> u32{
+	let desired_height : f64 = 300.0;
+	let disk_size_3 : u32 = 3;
+	let disk_size_5 : u32 = 5; 
 	
 	let img = image::open(path).unwrap();
 	
@@ -10,29 +13,29 @@ pub fn count_eyes(path : &str) -> u32{
 	
 	img_vec.load_image(img);
 	
-	img_vec.save_image("cropped.bmp");
+	img_vec.save_image("dice_step_1.bmp");
 	
 	img_vec.grayscale();
 	
 	img_vec.invert();
 	let mut bw_vec = img_vec.treshold(210);
 	
-	let ratio :f64 = 300.0/f64::from(bw_vec.height);
+	let ratio :f64 = desired_height / f64::from(bw_vec.height);
 	bw_vec.resize(ratio);
 	
 	let mut label_vec = ImgLabelMat::new();
-	img_vec.save_image("testPre.bmp");
+	img_vec.save_image("dice_step_2.bmp");
 	
-	let window = create_disk(3);
-	let window2 = create_disk(5);
+	let window = create_disk(disk_size_3);
+	let window2 = create_disk(disk_size_5);
 	
-	bw_vec.save_image("test1.bmp");
+	bw_vec.save_image("dice_step_3.bmp");
 	
 	bw_vec.morph_erode(&window);
-	bw_vec.save_image("test2.bmp");
+	bw_vec.save_image("dice_step_4.bmp");
 	
 	bw_vec.morph_dilate(&window2);
-	bw_vec.save_image("test3.bmp");
+	bw_vec.save_image("dice_end_result.bmp");
 	
 	label_vec.hoskop_coco(bw_vec.clone());
 
--- Visionb8/src/vision8b/img_hsv_mat.rs
+++ Visionb8/src/vision8b/img_hsv_mat.rs
@@ -17,10 +17,12 @@ pub struct ImgHSVMat {
 
 impl ImgHSVMat {
 	pub fn new() -> ImgHSVMat {
+		let no_size = 0;
+
 		ImgHSVMat {
 			image_matrix: Vec2d::new(),
-			width: 0,
-			height: 0,
+			width: no_size,
+			height: no_size,
 		}
 	}
 
@@ -76,31 +78,49 @@ fn color_min (red : f64, green : f64, blue : f64) -> f64{
 
 fn calculate_hue (red_accent : f64, green_accent : f64, blue_accent : f64, delta : f64, max : f64) -> u8{
 	let max_value = 255.0;
+	let invalid_delta = 0.0;
 
-	if delta == 0.0 {return 0};
+	let degree_multiplier = 60.0;
+	let full_degree_circle = 360.0;
+
+	let constant1 = 6.0;
+	let constant2 = 2.0;
+	let constant3 = 4.0;
+
+	let no_hue = 0;
+
+	if delta == invalid_delta {return no_hue};
 
 	if max == red_accent {
+
 		let intermediate = (green_accent - blue_accent) / delta;
-		let result = (intermediate % 6.0) * 60.0;
-		return ((result / 360.0) * max_value) as u8;
+		let result = (intermediate % constant1) * degree_multiplier;
+		return ((result / full_degree_circle) * max_value) as u8;
+		
 	}else if max == green_accent {
+
 		let intermediate = (blue_accent - red_accent) / delta;
-		let result = (intermediate + 2.0) * 60.0;
-		return ((result / 360.0) * max_value) as u8;
+		let result = (intermediate + constant2) * degree_multiplier;
+		return ((result / full_degree_circle) * max_value) as u8;
+
 	}else if max == blue_accent {
+
 		let intermediate = (red_accent - green_accent) / delta;
-		let result = (intermediate + 4.0) * 60.0;
-		return ((result / 360.0) * max_value) as u8;
+		let result = (intermediate + constant3) * degree_multiplier;
+		return ((result / full_degree_circle) * max_value) as u8;
+
 	}
 
-	return 0;
+	return no_hue;
 }
 
 fn calculate_saturation(delta : f64, max : f64) -> u8{
 	let max_value = 255.0;
+	let invalid_max = 0.0;
+	let no_saturation = 0;
 
-	if max == 0.0 {
-		return 0;
+	if max == invalid_max {
+		return no_saturation;
 	}else {
 		return ((delta / max) * max_value) as u8;
 	}
@@ -108,16 +128,21 @@ fn calculate_saturation(delta : f64, max : f64) -> u8{
 
 pub fn convert_rgb_pixel_to_hsv(rgb : &image::Rgba<u8>) -> HSVPixel{
 	let max_value : f64 = 255.0;
+	let no_value = 0;
+
+	let red_index = 0;
+	let green_index = 1;
+	let blue_index = 2;
 
 	let mut hsv_pixel = HSVPixel {
-		hue: 0 as u8, 
-		saturation: 0 as u8, 
-		value: 0 as u8
+		hue: no_value as u8, 
+		saturation: no_value as u8, 
+		value: no_value as u8
 	};
 
-	let ra : f64 = rgb[0] as f64 / max_value;
-	let ga : f64 = rgb[1] as f64 / max_value;
-	let ba : f64 = rgb[2] as f64 / max_value;
+	let ra : f64 = rgb[red_index] as f64 / max_value;
+	let ga : f64 = rgb[green_index] as f64 / max_value;
+	let ba : f64 = rgb[blue_index] as f64 / max_value;
 
 	let max = color_max(ra, ga, ba);
 	let min = color_min(ra, ga, ba);
